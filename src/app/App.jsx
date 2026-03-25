9b87b02c226c9840140b9c11c0c12a7e32afe50d
marcss-bnajera <mbeteta-2024227@kinal.edu.gt>
2026-03-25 11:56:16 -0600

    primer hola mundo

---
+++ src/app/App.jsx
@@ -0,0 +1,15 @@
+export const App = () => {
+  return (
+    <>
+      <h1 className="text-3xl">Hola mundo</h1>
+        <p>Lorem ipsum, dolor sit amet consectetur
+        adipisicing elit. Enim deleniti voluptates
+        dignissimos accusamus minus earum eum optio
+        cumque impedit labore vero ipsa temporibus
+        similique, rerum id! Eius rerum accusantium iusto.
+        </p>
+    </>
+  )
+}
+
+export default App;
